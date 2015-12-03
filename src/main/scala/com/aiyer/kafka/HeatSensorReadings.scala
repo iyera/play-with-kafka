dd317cb0f5528f3c503806f4111c17d7e686ab7c
Anand Iyer <aiyer@Anand-Iyer-MBA.local>
2015-12-03 15:38:23 -0800

    use json4s

--- src/main/scala/com/aiyer/kafka/HeatSensorReadings.scala
+++ src/main/scala/com/aiyer/kafka/HeatSensorReadings.scala
@@ -3,37 +3,42 @@
  */
 package com.aiyer.kafka
 
-import play.api.libs.json.Json
+// Read the "Serialization" section in https://github.com/json4s/json4s
+// Case classes can be serialized and deserialized.
+import org.json4s.jackson.Serialization
+
+import org.json4s.NoTypeHints
 
 object HeatSensorReadings {
+
+  implicit val formats = Serialization.formats(NoTypeHints)
+
   def main(args: Array[String]): Unit = {
     val topicName = args(0)
 
     println("Lets get the party started")
 
     val producer = Producer[String](topicName)
-    val message = Message(2, 45.0, "Lets start you off with a light load")
+    val message = SensorReading(2, 45.0, "Lets start you off with a light load")
 
-    implicit val writes = Json.writes[Message]
 
     var counter = 0
     while (true) {
 
-      val message = Message((Math.random()*10000).toInt, Math.random()*200, "Some text to increase message size. Lets fill those pipes up!!")
-
-      implicit val writes = Json.writes[Message]
+      val message = SensorReading((Math.random()*10000).toInt, Math.random()*200, "Some text to increase message size. Lets fill those pipes up!!")
 
       counter += 1
 
-      val jsonEvent = Json.toJson(message).toString
       if(counter % 5000 == 0) {
-        println("Lets write an event to Kafka: " + jsonEvent)
+        println("Lets write an event to Kafka: " + message.sensorId)
         counter = 0
       }
-      producer.send(jsonEvent )
+      producer.send(Serialization.write(message))
       //Thread.sleep(1)
     }
   }
+
+
 }
 
-case class Message(sensorId: Int, temp: Double, desc: String)
+case class SensorReading(sensorId: Int, temp: Double, desc: String)
